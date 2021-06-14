f4a801022fe5073d21a071fb1f922e85d44ce209
Gustavo <gustavonikov@gmail.com>
2021-06-14 20:35:56 -0300

    update&add: adjusts on home and app, added styles.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { ThemeProvider } from '@material-ui/core'
 import theme from '../styles/themes/theme'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
+import { AppContainer } from '../styles/pages/_app.style'
 
 function MyApp({ Component, pageProps }) {
 	return (
@@ -13,9 +15,12 @@ function MyApp({ Component, pageProps }) {
 				<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,600;1,700&display=swap" rel="stylesheet" />
 				<link href="/fonts/tw-icons/css/treinaweb-icons.css" rel="stylesheet" />
 			</Head>
-			<ThemeProvider theme ={theme}>
-				<Header />
-				<Component {...pageProps} />
+			<ThemeProvider theme={theme}>
+				<AppContainer>
+					<Header />
+					<Component {...pageProps} />
+					<Footer />
+				</AppContainer>
 			</ThemeProvider>
 		</>
 	)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,25 @@
 import PageTitle from '../components/PageTitle'
 import SafeEnv from '../components/SafeEnv/index'
 import UserInfo from '../components/UserInfo'
+import TextFieldMask from '../components/TextFieldMask'
+import { Button, Typography, Container, CircularProgress } from '@material-ui/core'
+import { FormElementsContainer, ProfessionalsPaper, ProfessionalsContainer } from '../styles/pages/index.style'
+
+import useHome from '../hooks/pages/home/useHome'
 
 export default function Home() {
+	const {
+		cep,
+		setCep,
+		validCep,
+		searchDiarists,
+		diarists,
+		error,
+		searchDone,
+		loading,
+		remainingDiarists
+	} = useHome()
+
 	return (
 		<div>
 			<SafeEnv />
@@ -11,19 +28,90 @@ export default function Home() {
 				subtitle="Preencha seu endereço e veja todos os profissionais da sua localidade"
 			/>
 
-			<UserInfo
-				pictureUrl="https://github.com/gustavonikov.png"
-				name="Gustavo Chaves"
-				rating={4}
-				description="João Pessoa - PB"
-			/>
+			<Container>
+				<FormElementsContainer>
+					<TextFieldMask
+						mask="99.999-999"
+						label="Digite seu CEP"
+						fullWidth
+						variant="outlined"
+						value={cep}
+						onChange={({ target }) => setCep(target.value)}
+					/>
+					{
+						error && <Typography color="error">{error}</Typography>
+					}
+					<Button
+						variant="contained"
+						color="secondary"
+						sx={{ width: '220px' }}
+						disabled={!validCep || loading}
+						onClick={() => searchDiarists(cep)}
+					>
+						{loading ? <CircularProgress size={20} /> : 'Buscar'}
+					</Button>
+				</FormElementsContainer>
 
-			<UserInfo
-				name="Victoria Nunes"
-				rating={3}
-				description="João Pessoa - PB"
-			/>
+				{searchDone && (
+					diarists.length > 0 ?
+					(
+						<ProfessionalsPaper>
+							<ProfessionalsContainer>
+								{
+									diarists.map((diarist, index) => {
+										return (
+											<UserInfo
+												key={index}
+												pictureUrl={diarist.foto_usuario}
+												name={diarist.nome_completo}
+												rating={diarist.reputacao}
+												description={diarist.cidade}
+											/>
+										)
+									})
+								}
 
+							</ProfessionalsContainer>
+
+							<Container sx={{ textAlign: 'center' }}>
+								{
+									remainingDiarists > 0 && (
+										<Typography sx={{ mt: 5 }}>
+											... e mais {remainingDiarists} 
+											{
+												remainingDiarists > 1 ?
+													'profissionais atendem'
+													:
+													'profissional atende'
+											}
+
+											{' ao seu endereço'}
+										</Typography>
+									)
+								}
+
+								<Button
+									variant="contained"
+									color="secondary"
+									sx={{ mt: 5 }}
+								>
+									Contratar um profissional
+								</Button>
+							</Container>
+						</ProfessionalsPaper>
+
+					)
+					:
+					(
+						<Typography align='center' color="textPrimary" sx={{ mt: 5 }}>
+							Ainda não temos nenhuma diarista disponível em sua região
+						</Typography>
+					)
+				)
+				}
+
+
+			</Container>
 		</div>
 	)
 }
